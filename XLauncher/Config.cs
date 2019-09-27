7621af56a2833a6938ba5ba92a73571030220b4b
XCVGVCX <xcvgvcx@gmail.com>
2019-09-26 17:27:42 -0700

    Add project files.

---
+++ XLauncher/Config.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Diagnostics;
+using System.Globalization;
+using System.IO;
+
+namespace XLauncher
+{
+
+    /// <summary>
+    /// Config object; loaded from startup file
+    /// </summary>
+    class Config
+    {
+        //title=
+        public string WindowTitle { get; private set; } = "Game Launcher";
+        //start=
+        public string GamePath { get; private set; } = "game.exe"; //dumb default
+        //args=
+        public string GameArgs { get; private set; } = null;
+        //opts=
+        public string OptionsPath { get; private set; } = null;
+        //help=
+        public string HelpPath { get; private set; } = null;
+        //image=
+        public string ImagePath { get; private set; } = null;
+        //music=
+        public string MusicPath { get; private set; } = null;
+        //close=
+        public bool CloseOnPlay { get; private set; } = true;
+
+        public Config(string path)
+        {
+            if (File.Exists(path))
+                ParseFile(File.ReadAllLines(path));            
+        }
+
+        private void ParseFile(string[] lines)
+        {
+            foreach(string line in lines)
+            {
+                try
+                {
+                    if (string.IsNullOrWhiteSpace(line)) //skip empty lines
+                        continue;
+
+                    string trimmedLine = line.Trim();
+                    if (trimmedLine.StartsWith(";", StringComparison.OrdinalIgnoreCase) ||
+                        trimmedLine.StartsWith("[", StringComparison.OrdinalIgnoreCase)) //skip comments and headers
+                        continue;
+
+                    int indexOfEquals = trimmedLine.IndexOf('=');
+                    if (indexOfEquals == -1)
+                        continue;
+
+                    string[] splitLine = new string[] { trimmedLine.Substring(0, indexOfEquals), trimmedLine.Substring(indexOfEquals + 1, trimmedLine.Length - indexOfEquals - 1) };
+                    ParseLine(splitLine);
+                }
+                catch(Exception ex)
+                {
+                    Debug.WriteLine($"Parse error on \"{line}\": {ex.GetType().Name}");
+                }
+            }
+        }
+
+        private void ParseLine(string[] line)
+        {
+            string key = line[0].ToLower(CultureInfo.InvariantCulture);
+            switch (key)
+            {
+                case "title":
+                    WindowTitle = line[1];
+                    break;
+                case "start":
+                    GamePath = line[1];
+                    break;
+                case "args":
+                    GameArgs = line[1];
+                    break;
+                case "options":
+                    OptionsPath = line[1];
+                    break;
+                case "help":
+                    HelpPath = line[1];
+                    break;
+                case "image":
+                    ImagePath = line[1];
+                    break;
+                case "music":
+                    MusicPath = line[1];
+                    break;
+                case "close":
+                    CloseOnPlay = bool.Parse(line[1]);
+                    break;
+            }
+        }
+    }
+}
---
+++ XLauncher/MainWindow.xaml.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Windows;
+using System.Windows.Media.Imaging;
+
+namespace XLauncher
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        private Config Config;
+
+        private string CurrentDirectory => Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location);
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+
+                //get startup args
+                string configPath = Path.Combine(CurrentDirectory, "startup.ini");
+                Config = new Config(configPath);
+
+                //do stuff
+                Title = Config.WindowTitle;
+
+                //hide unused buttons
+                if(string.IsNullOrEmpty(Config.HelpPath))
+                {
+                    ButtonHelp.Visibility = Visibility.Hidden;
+                }
+
+                if(string.IsNullOrEmpty(Config.OptionsPath))
+                {
+                    ButtonOpts.Visibility = Visibility.Hidden;
+                }
+
+                //probably a gross hack...
+                if (!string.IsNullOrEmpty(Config.ImagePath))
+                {
+                    string imagePath = Path.Combine(CurrentDirectory, Config.ImagePath);
+                    if (File.Exists(imagePath))
+                    {
+                        StartupImage.Source = new BitmapImage(new Uri(imagePath));
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Failed to initialize launcher! {(ex.GetType().Name)}: {ex.Message}");
+
+                Debug.WriteLine("Failed to initialize launcher!");
+                Debug.WriteLine(ex);
+                Close();
+            }
+        }
+
+        
+
+        private void ButtonExit_Click(object sender, RoutedEventArgs e)
+        {
+            //Application.Current.Shutdown();
+            Close();
+            //Environment.Exit(0);
+        }
+
+        private void ButtonHelp_Click(object sender, RoutedEventArgs e)
+        {
+            //System.Diagnostics.Debug.Write(helpPath);
+            try
+            {
+                Process.Start(Path.Combine(CurrentDirectory, Config.HelpPath));
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Failed to open help! {(ex.GetType().Name)}: {ex.Message}");
+
+                Debug.WriteLine("Failed to open help!");
+                Debug.WriteLine(ex);
+                //Environment.Exit(0);
+            }
+        }
+
+        private void ButtonOpts_Click(object sender, RoutedEventArgs e)
+        {
+            //System.Diagnostics.Debug.Write(optsPath);
+            try
+            {
+                Process.Start(Path.Combine(CurrentDirectory, Config.OptionsPath));
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Failed to open options! {(ex.GetType().Name)}: {ex.Message}");
+
+                Debug.WriteLine("Failed to open options!");
+                Debug.WriteLine(ex);
+                //Environment.Exit(0);
+            }
+        }
+
+        private void ButtonPlay_Click(object sender, RoutedEventArgs e)
+        {
+            //System.Diagnostics.Debug.Write(gamePath);
+            try
+            {
+                string fullGamePath = Path.Combine(CurrentDirectory, Config.GamePath);
+                Process gameProcess = new Process();
+                gameProcess.StartInfo.FileName = fullGamePath;
+                gameProcess.StartInfo.WorkingDirectory = Path.GetDirectoryName(fullGamePath);
+                if(!string.IsNullOrEmpty(Config.GameArgs))
+                    gameProcess.StartInfo.Arguments = Config.GameArgs;
+
+                gameProcess.Start();
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Failed to open game! {(ex.GetType().Name)}: {ex.Message}");
+
+                Debug.WriteLine("Failed to open game!");
+                Debug.WriteLine(ex);
+                //Environment.Exit(0);
+            }
+
+            //probably want to close on play
+            if(Config.CloseOnPlay)
+                Close();
+        }
+    }
+}
